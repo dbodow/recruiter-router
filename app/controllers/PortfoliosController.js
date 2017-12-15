0cb426b9a0140da390ebdf3d80343bb07116a77d
aahmad94 <aka5vm@virginia.edu>
2017-12-14 20:27:36 -0800

    add portfolio page city wallpaper

--- app/controllers/PortfoliosController.js
+++ app/controllers/PortfoliosController.js
@@ -1,26 +1,31 @@
-// <<<<<<< HEAD
 const mongoose = require("mongoose");
-
 const portfoliosController = {};
+const portfolios = require("../models/portfolio");
 
 portfoliosController.loadPorfolio = (req, res) => {
-  const user = req.params.user;
-  const company = req.params.company;
-  res.render('portfolio', { user: user, company: company });
+  const paramsUser = req.params.user;
+  const paramsCompany = req.params.company.toLowerCase().replace( /\W/g, '' );
+  portfolios
+    .findOne({username: paramsUser})
+    .exec()
+    .then((result) => {
+       const port = result;
+       const analytics = port.analytics;
+      //  console.log(analytics[0].companyName);
+       let company;
+       for (let i = 0; i < analytics.length; i++) {
+         const analyticsObjCompanyName =
+         analytics[i].companyName.toLowerCase().replace( /\W/g, '' );
+         if (analyticsObjCompanyName ===
+           paramsCompany) {
+             company = analytics[i].companyName;
+             break;
+          }
+       }
+      console.log("---ANALYTICS COMPANY---");
+      console.log(company);
+      res.render('portfolio', { port: port, company: company });
+    });
 };
 
 module.exports = portfoliosController;
-// =======
-// const Portfolio = require("../models/portfolio");
-// const mongoose = require("mongoose");
-//
-// export const getPortfolio = async (req, res) => {
-//   try {
-//     return res.status(200).json({ portfolio: await Portfolio.find() });
-//   } catch (e) {
-//     return res
-//       .status(e.status)
-//       .json({ error: true, message: "Portfolio not found" });
-//   }
-// };
-// >>>>>>> master
