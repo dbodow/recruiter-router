df51c5bec6c0a0505783da101bcaa7ae96b7b686
David Bodow <dbodow@gmail.com>
2017-12-18 06:51:31 -0800

    Add splash page

--- app/frontend/components/app.jsx
+++ app/frontend/components/app.jsx
@@ -4,6 +4,7 @@ import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
 
 import GreetingContainer from "./greeting/greeting_container";
 import SessionFormContainer from "./session_form/session_form_container";
+import Splash from "./splash/splash";
 import PortfolioBuilderContainer from "./portfolio_builder/portfolio_builder_container";
 import PortfolioManagerContainer from "./portfolio_manager/portfolio_manager_container";
 import NavBar from "./navbar/navbar";
@@ -25,6 +26,7 @@ const App = () => (
       </div>
     </header>
     <Switch>
+      <Route exact path="/" component={Splash} />
       <AuthRoute path="/login" component={SessionFormContainer} />
       <AuthRoute path="/register" component={SessionFormContainer} />
       <ProtectedRoute path="/builder" component={PortfolioBuilderContainer} />
